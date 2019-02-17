66c31d11a9723023c110b16837208b2f486e2167
Lom4ick <47662868+Lom4ick@users.noreply.github.com>
2019-02-17 07:50:53 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.groupqa1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Name of your future job-->");
+        String name = scanner.nextLine();
+        System.err.println("* * * *    * * * *      *      *       *");
+        System.err.println("*          *     *     * *     * *   * *");
+        System.err.println("* *        * * * *    *   *    *   *   *");
+        System.err.println("*          *         * * * *   *       *");
+        System.err.println("* * * *    *        *       *  *       *");
+    }
+}
